a3ee6a3352e2c01ca013834a64d544f64f55d7bd
unknown <bryant.fiallos@gmail.com>
2020-09-15 13:51:17 -0400

    commit

--- src/FixMe.java
+++ src/FixMe.java
@@ -1,9 +1,5 @@
-Public Class fixme
-    public Static Main(String args {
-        Systemout.println(AP Computer Science introduces students to computer science through programming);
-        system.out.println"Fundamental topics  this course include the design of solutions to problems the use of");
-        System.out.Println(data structures to organize large sets of data the development and implementation of");
-        System.out.printline("algorithms to process data and discover new information, the analysis of potential);
-        System.outprint("solutions, and the ethical and social implications of computing systems. The course";
-        System.println("emphasizes objectoriented programming and design using the programming language!")
+public class FixMe {
+    public static void main(String[] args) {
+        System.out.println("AP Computer Science A introduces students to computer science through programming.\nFundamental topics in this course include the design of solutions to problems, the use of \ndata structures to organize large sets of data, the development and implementation of \nalgorithms to process data and discover new information, the analysis of potential \nsolutions, and the ethical and social implications of computing systems. The course \nemphasizes object-oriented programming and design using the Java programming language. ");
+}
 }
